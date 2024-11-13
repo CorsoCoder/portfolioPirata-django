eedd2c7ec7f30add722db2f31b5ea37a5f830baf
corso <45120484+CorsoCoder@users.noreply.github.com>
2024-11-13 01:36:22 +0100

    Update settings.py

--- Corsario/settings.py
+++ Corsario/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zp#vb3c=s9*)$=qp#k(4m3fev%n_!^0j#-w_c8@wsjb-1)n0tm'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
